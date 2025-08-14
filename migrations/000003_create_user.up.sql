7e568777ec3e473e542de19fb7513e29a497742d
Ivan Danko <i.danko@bars.group>
2025-08-14 21:13:42 +0700

    iter14 не нужна табл users

--- migrations/000003_create_user.up.sql
+++ migrations/000003_create_user.up.sql
@@ -5,18 +5,18 @@ CREATE TABLE IF NOT EXISTS users (
 );
 ALTER TABLE urls ADD COLUMN IF NOT EXISTS user_id INT;
 COMMIT;
-
-DO $$
-    BEGIN
-        IF NOT EXISTS (
-            SELECT 1
-            FROM information_schema.table_constraints
-            WHERE table_name = 'urls'
-              AND constraint_type = 'FOREIGN KEY'
-              AND constraint_name = 'fk_user_id'
-        ) THEN
-            ALTER TABLE urls
-                ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
-        END IF;
-    END
-$$;
+-- todo может быть и не нужна табл users... из за внешнего ключа тест не проходит по inc10
+-- DO $$
+--     BEGIN
+--         IF NOT EXISTS (
+--             SELECT 1
+--             FROM information_schema.table_constraints
+--             WHERE table_name = 'urls'
+--               AND constraint_type = 'FOREIGN KEY'
+--               AND constraint_name = 'fk_user_id'
+--         ) THEN
+--             ALTER TABLE urls
+--                 ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
+--         END IF;
+--     END
+-- $$;
