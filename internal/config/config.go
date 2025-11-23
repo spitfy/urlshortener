5ee6e9d004ca0acdcb3bf1f4590d7f60d1ae3b49
Ivan Danko <i.danko@bars.group>
2025-11-23 14:06:13 +0700

    iter24 conf priority

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,12 +3,13 @@ package config
 
 import (
 	"flag"
-	"github.com/caarlos0/env/v6"
+	"log"
+
 	audit "github.com/spitfy/urlshortener/internal/audit/config"
 	"github.com/spitfy/urlshortener/internal/config/db"
 	storageConf "github.com/spitfy/urlshortener/internal/repository/config"
-	"log"
 
+	"github.com/caarlos0/env/v6"
 	authConf "github.com/spitfy/urlshortener/internal/auth/config"
 	handlerConf "github.com/spitfy/urlshortener/internal/handler/config"
 	loggerConf "github.com/spitfy/urlshortener/internal/logger/config"
@@ -41,11 +42,6 @@ func GetConfig() *Config {
 	var conf = &Config{
 		Auth: authConf.Config{SecretKey: SecretKey},
 	}
-
-	if err := env.Parse(conf); err != nil {
-		log.Fatal(err)
-	}
-
 	flag.StringVar(&conf.Handlers.ServerAddr, "a", DefaultServerAddr, "address of HTTP server")
 	flag.StringVar(&conf.Service.ServerURL, "b", DefaultServerURL, "URL of HTTP server")
 	flag.StringVar(&conf.Logger.LogLevel, "l", DefaultLogLevel, "Logger level")
@@ -56,6 +52,10 @@ func GetConfig() *Config {
 	flag.BoolVar(&conf.Handlers.EnableHTTPS, "s", DefaultHTTPS, "Enable HTTPS server")
 
 	flag.Parse()
+
+	if err := env.Parse(conf); err != nil {
+		log.Fatal(err)
+	}
 
 	return conf
 }
